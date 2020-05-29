6b6baedafeeff5edb063d8230dc62ea488d5e55b
Zenahr Barzani <zenmatica@gmail.com>
2020-05-29 20:30:38 +0200

    added icons again

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -2,11 +2,11 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as React from 'react';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import HiraganaScreen from '../screens/HiraganaScreen';
+import KatakanaScreen from '../screens/KatakanaScreen';
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = 'Hiragana'; // The actual route, not just an abritrary string.
 
 export default function BottomTabNavigator({ navigation, route }) {
   // Set the header title on the parent stack navigator depending on the
@@ -17,18 +17,18 @@ export default function BottomTabNavigator({ navigation, route }) {
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
       <BottomTab.Screen
-        name="Home"
-        component={HomeScreen}
+        name="Hiragana"
+        component={HiraganaScreen}
         options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          title: '平仮名 (ひらがな)',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
         }}
       />
       <BottomTab.Screen
-        name="Links"
-        component={LinksScreen}
+        name="Katakana"
+        component={KatakanaScreen}
         options={{
-          title: 'Resources',
+          title: '片仮名 (カタカナ)',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
         }}
       />
@@ -40,9 +40,9 @@ function getHeaderTitle(route) {
   const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
   switch (routeName) {
-    case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
+    case 'Hiragana':
+      return 'Hiragana Chart';
+    case 'Katakana':
+      return 'Katakana Chart';
   }
 }
